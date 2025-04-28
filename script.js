7946eeee0aa5687aedd5b1c83852f0d018d9699b
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-04-28 21:00:42 +0300

    Designed the contact modal

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-function chevronClicked(chevronType){
+function chevronClicked(chevronType) {
     var container = document.querySelector("#my-projects-page-main-div");
 
-    if(chevronType == "left"){
-        container.scrollBy({  left: container.scrollWidth * (-1/3),
-            behavior: 'smooth'});
+    if (chevronType == "left") {
+        container.scrollBy({
+            left: container.scrollWidth * (-1 / 3),
+            behavior: 'smooth'
+        });
     } else {
-        container.scrollBy({  left: container.scrollWidth * (1/3),
-            behavior: 'smooth'});
+        container.scrollBy({
+            left: container.scrollWidth * (1 / 3),
+            behavior: 'smooth'
+        });
     }
 }
 
 document.querySelectorAll(".app-div").forEach(div => {
-    div.onclick = function() {
+    div.onclick = function () {
         var divAppName = div.getAttribute("data-appname");
         window.location.href = "../projects/" + divAppName + "/" + divAppName + ".html";
     };
-  });
+});
+
+document.querySelector('#close-contact-modal-button').onclick = function () {
+    document.querySelector('#contact-modal').style.opacity = '0';
+    setTimeout(() => {
+          document.querySelector('#contact-modal').style.display='none';
+    }, 300);
+};
+
+document.querySelectorAll("a.contact-link").forEach(a => {
+    a.onclick = () => {
+        document.querySelector('#contact-modal').style.display='flex';
+        requestAnimationFrame(()=> document.querySelector('#contact-modal').style.opacity = '1');
+    };
+});
