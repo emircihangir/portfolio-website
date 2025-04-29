df3ec936d1c2e5d2e3822cfe2a555e907829dfa0
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-04-29 18:10:34 +0300

    Created components.js

---
+++ components.js
@@ -0,0 +1,23 @@
+class ContactModal extends HTMLElement {
+    connectedCallback() {
+      this.innerHTML = `
+        <div id="contact-modal">
+            <div style="padding: 50px; background-color: white;border: 1px solid black; position: inherit;">
+                <div id="close-contact-modal-button"></div>
+
+                <p style="font-size: 48px; position: relative; z-index: 99; font-weight: 600;">Contact</p>
+                <div style="width: 300px; height: 70px; background-color: var(--main-color);position: relative; bottom: 40px;"></div>
+                <p>Call: <a href="tel:+905332058243">+90-533-205-8243</a></p>
+                <p>Write: <a href="mailto:m.emircihangir@gmail.com">m.emircihangir@gmail.com</a></p>
+                <div style="display: flex; justify-content: center;">
+                    <div style="background-image: url('/assets/images/github-mark.svg');" class="logo-div" onclick="window.open('https://github.com/emircihangir', '_blank')"></div>
+                    <div style="background-image: url('/assets/images/linkedin-logo.png');" class="logo-div" onclick="window.open('https://www.linkedin.com/in/emir-cihangir-683146313/', '_blank')"></div>
+                    <div style="background-image: url('/assets/images/instagram-logo.svg');" class="logo-div" onclick="window.open('https://www.instagram.com/cihangirmuhammedemir/', '_blank')"></div>
+                </div>
+            </div>
+        </div>
+      `;
+    }
+  }
+  
+  customElements.define('contact-modal', ContactModal);
