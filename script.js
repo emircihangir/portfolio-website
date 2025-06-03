242078ec26c5ea92346f7c07096a65593a1392c3
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-06-03 16:07:59 +0300

    Temporarily change the scroll length to 1/3

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ function chevronClicked(chevronType) {
 
     if (chevronType == "left") {
         container.scrollBy({
-            left: container.scrollWidth * (-1 / 4),
+            left: container.scrollWidth * (-1 / 3),
             behavior: 'smooth'
         });
     } else {
         container.scrollBy({
-            left: container.scrollWidth * (1 / 4),
+            left: container.scrollWidth * (1 / 3),
             behavior: 'smooth'
         });
     }
